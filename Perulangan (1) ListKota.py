fcbf8895999ed3226505219fb8f74ec7ce20f4c4
Aria Dewangga <ariadewangga098@gmail.com>
2025-12-10 10:51:35 +0700

    Update Perulangan (1) ListKota.py

--- Perulangan (1) ListKota.py
+++ Perulangan (1) ListKota.py
@@ -2,4 +2,16 @@ list_Kota =[
     'Jakarta','Surabaya','depok','Bekasi','Solo','Jogjakarta','semarang','Makasar'
     ]
 for kota in list_Kota:
-    print(kota)
+   print(kota)
+else:
+    print('Tidak ada lagi item yang tersisa')
+    kotaYangDicari=input('ketik nama kota yang kamu cari:')
+    for i, kota in enumerate(list_Kota):
+        # kita ubah katanya ke lowercase agar
+        # menjadi case insensitive
+        if kota.lower()==kotaYangDicari.lower():
+            print("kota yang anda cari berada pada indeks",i)
+            break
+        else:
+            print('Maaf, kota yang anda cari tidak ada')  
+    
